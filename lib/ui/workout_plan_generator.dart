f38f24ceec3c10f294b347b824d588c0f6205478
flores912 <flores.jose.andres.912@gmail.com>
2023-12-31 16:51:06 -0700

    fixed some bugs

--- lib/ui/workout_plan_generator.dart
+++ lib/ui/workout_plan_generator.dart
@@ -166,7 +166,7 @@ class WorkoutPlanGeneratorState extends State<WorkoutPlanGenerator> {
     final chat = await OpenAI.instance.chat.create(
       responseFormat: {"type": "json_object"},
       model: "gpt-3.5-turbo-1106",
-      temperature: 0.3,
+      temperature: 0.2,
       n: 1,
       messages: [
         systemMessageRequest,
